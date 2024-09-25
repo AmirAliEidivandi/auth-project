3a12eab91a9e666bd5e1aa2ec6f27ae5596969cf
AmirAliEidivandi <amirah648887@gmail.com>
2024-09-25 17:14:42 +0330

    add regex for username and email

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,9 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
 
 export class RegisterDto {
   @IsString()
   @IsNotEmpty()
+  @Matches(/^(?!.*[_.]{2})[a-zA-Z0-9._]{3,20}(?<![_.])$/, {
+    message:
+      'Username must be between 3-20 characters, and may contain letters, numbers, underscores, and dots. It cannot start or end with a dot or underscore, nor have consecutive dots/underscores.',
+  })
   @ApiProperty({
     type: String,
     description: 'The new username for registration',
@@ -38,6 +48,9 @@ export class RegisterDto {
 
   @IsEmail()
   @IsNotEmpty()
+  @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+    message: 'Email must be a valid email address',
+  })
   @ApiProperty({
     type: String,
     description: 'Email of the user',
